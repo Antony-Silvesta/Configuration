ce186c2a5e20cdd75b0dc838a5e1bcbc07551f76
Antony-Silvesta <antonysilvesta74@gmail.com>
2024-11-23 23:17:17 +0530

    Updated Fully

--- test.sh
+++ test.sh
@@ -3,10 +3,10 @@ echo "Starting Selenium Tests..."
 # Detect operating system
 if [[ "$OSTYPE" == "linux-gnu"* || "$OSTYPE" == "darwin"* ]]; then
     # Unix-based paths (e.g., GitHub Actions)
-    script_path="/home/runner/work/LoginTest/LoginTest"
+    script_path="/home/runner/work/Configuration/configuration"
 elif [[ "$OSTYPE" == "msys"* || "$OSTYPE" == "win32"* ]]; then
     # Windows-based paths
-    script_path="C:/Users/jebas/LoginTest"
+    script_path="C:/Users/shinba/Configuration"
 else
     echo "Unsupported OS type: $OSTYPE"
     exit 1
@@ -16,6 +16,7 @@ export PYTHONPATH="$script_path"
 # Print the paths for debugging
 echo "Using script path: $script_path"
 # Run each Selenium test script
-python "$script_path/scripts/setup_mongodb.py"
-python "$script_path/login_testcases/invlid.py"
-echo "All tests completed."
+python "$script_path/configfile/config.py"
+pytest "$script_path/login_testcases/test_invalid_login.py" 
+pytest "$script_path/login_testcases/test_valid_login.py" 
+echo "All tests completed." 
